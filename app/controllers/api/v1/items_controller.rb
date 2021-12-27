27ccc51a2a7f0f71ec5914a5ec7e08f79bace640
Miguel Dacanay <migueldacanay@Miguels-MacBook-Air.local>
2021-12-26 23:43:16 -0800

    item create json builder done

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,9 +1,36 @@
 module Api
     module V1
         class ItemsController < ApplicationController
+            skip_before_action :verify_authenticity_token
+
             def show
                 @item = Item.find(params[:id])
-                render json: @item
+                render json: @item, status: 200
+            end
+
+            def create 
+                @item = user.items.new(item_params)
+                if @item.save
+                    render :create, status: 200
+                else
+                    render json: {error: @user.errors.message}, status: 422
+                end
+            end
+
+            def update
+            end
+
+            def destroy
+            end
+
+            private
+
+            def user
+                @user ||= User.find(params[:user_id])
+            end
+
+            def item_params
+                params.require(:item).permit(:title, :description)
             end
         end
     end
---
+++ app/views/api/v1/items/create.json.jbuilder
@@ -0,0 +1,2 @@
+json.item @item, :title, :description
+json.user_id @item.user_id
